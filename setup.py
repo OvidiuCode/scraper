d269bca22b69a7b8138c2b286ab4d4fc8a35fad6
Ovidiu Rusu <ovidiumax1994@gmail.com>
2019-01-13 16:38:06 +0200

    Init package

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="scraper",
+    version="0.0.1",
+    author="Ovidiu Rusu",
+    author_email="ovidiumax1994@gmail.com",
+    description="Web information scrapers",
+    url="git@github.com:OvidiuCode/scraper.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
